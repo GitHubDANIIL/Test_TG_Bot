01c1379de61190b04a4c71e3f43bb445ac531b73
Васильев Даниил <oni8009@gmail.com>
2024-05-28 16:21:02 +0300

    28.05.2024 Бот реагирует на команду /start

---
+++ src/main/java/com/Raphael/WB_TG_Bot/WbTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.Raphael.WB_TG_Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Raphael/WB_TG_Bot/WbTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Raphael.WB_TG_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
